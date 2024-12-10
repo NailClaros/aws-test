e7ec35401fa180930b964ad28cad526509685592
nailc <nailclaros@gmail.com>
2024-12-10 17:59:36 -0500

    images and audio

--- file.py
+++ file.py
@@ -1,6 +1,14 @@
 import boto3
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 s3 = boto3.client('s3')
 
 S3_BUCKET = os.getenv('S3_BUCKET')
-s3.upload_file('img.png', S3_BUCKET, 'test.png')
+if not S3_BUCKET:
+    raise ValueError("S3_BUCKET environment variable is not set!")
+
+s3.upload_file('ex.wav', S3_BUCKET, 'ex.wav')
+s3.upload_file('img.png', S3_BUCKET, 'ex.png ..')
+print("File uploaded successfully!")
